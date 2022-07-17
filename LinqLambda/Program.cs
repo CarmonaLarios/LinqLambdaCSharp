8d22a541a70100f570638bf5e37662b92db19364
Renan Larios <renan.larios@alumisoft.com.br>
2022-07-16 22:49:12 -0300

    Initial files

---
+++ LinqLambda/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var query= from num in salarios where num>1000 select num;
+            //foreach (var i in query)
+            //{
+            //    Console.WriteLine(i.ToString());
+            //}
+
+            double[] salarios = { 300 ,500,10000};
+            List<Salario> personSalario = new List<Salario>();
+
+            foreach (var s in salarios)
+            {
+                personSalario.Add(new Salario(s));
+            }
+
+            var query = from num in personSalario where num.getSalario() > 100 select num;
+
+            foreach (var i in query)
+            {
+                Console.WriteLine(i.getSalario());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinqLambda/Salario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLambda
+{
+    class Salario
+    {
+        private double salarioMes;
+
+        public Salario(double _salarioMes)
+        {
+         salarioMes = _salarioMes;
+        }
+
+       public void setSalario(double _salarioMes)
+        {
+            salarioMes = _salarioMes;
+        }
+
+        public double getSalario()
+        {
+            return salarioMes;
+        }
+
+    }
+}
